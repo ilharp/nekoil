c4df364eebcbcf17f4fc4ae07de4a2db42df6b14
Il Harper <hi@ilharper.com>
2025-03-28 01:09:39 +0800

    feat(core): update

---
+++ packages/core/src/index.ts
@@ -0,0 +1,88 @@
+import { Context, Schema } from 'koishi'
+
+export const name = 'nekoil-core'
+
+export interface Config {}
+
+export const Config: Schema<Config> = Schema.object({})
+
+declare module 'koishi' {
+  interface Tables {
+    cp_v1: ContentPackV1
+    cp_handle_v1: ContentPackHandleV1
+  }
+}
+
+export interface DatabaseBase {
+  created_time: Date
+  deleted: number
+  deleted_time: Date
+  deleted_reason: number
+}
+
+export const databaseBaseFields = {
+  created_time: {
+    type: 'timestamp',
+    nullable: false,
+  },
+
+  deleted: {
+    type: 'unsigned',
+    length: 1,
+    nullable: false,
+  },
+
+  deleted_time: {
+    type: 'timestamp',
+    nullable: true,
+  },
+
+  deleted_reason: {
+    type: 'unsigned',
+    length: 1,
+    nullable: false,
+  },
+}
+
+export interface ContentPackV1 extends DatabaseBase {
+  cpid: number
+  content_mode: number
+  content_zstd_v1: string
+  user_id: number
+  platform: number
+  cp_version: number
+}
+
+export interface ContentPackHandleV1 extends DatabaseBase {
+  cpid: number
+  handle_type: number
+  handle: string
+}
+
+export function apply(ctx: Context) {
+  ctx.model.extend('cp_v1', {
+    ...databaseBaseFields,
+  })
+
+  ctx.model.extend('cp_handle_v1', {
+    ...databaseBaseFields,
+
+    cpid: {
+      type: 'unsigned',
+      length: 64,
+      nullable: false,
+    },
+
+    handle_type: {
+      type: 'unsigned',
+      length: 1,
+      nullable: false,
+    },
+
+    handle: {
+      type: 'char',
+      length: 64,
+      nullable: false,
+    },
+  })
+}
