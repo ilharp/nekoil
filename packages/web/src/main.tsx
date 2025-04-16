30ac3763d247423e83f95ab5e725fe972d71ebe4
Il Harper <hi@ilharper.com>
2025-04-17 03:15:26 +0800

    feat(web): update cp

--- packages/web/src/main.tsx
+++ packages/web/src/main.tsx
@@ -1,6 +1,9 @@
 // Must be a standalone file and import at top
 import './preinit'
 
+import '@sym-app/components/styles/layout/index.scss'
+import '@sym-app/components/styles/theme-sym/index.scss'
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { App } from './App'
--- packages/web/src/pages/ContentPack/index.tsx
+++ packages/web/src/pages/ContentPack/index.tsx
@@ -1,1 +1,46 @@
-export const ContentPack = () => <h1>ContentPack</h1>
+import type { SymAioHost, SymMsgGroupCtx } from '@sym-app/components'
+import {
+  frRenderers,
+  SymAioCtxContext,
+  SymAioHostContext,
+  SymMsgGroupContext,
+  SymMsgVirtualList,
+  SymProvider,
+} from '@sym-app/components'
+import { useState } from 'react'
+import { Splash } from '../../components/Splash'
+import { ContentPackWithFull } from 'nekoil-typedef'
+
+export const ContentPack = () => {
+  const [loading, setLoading] = useState(true)
+  const [cp, setCp] = useState<ContentPackWithFull | undefined>(undefined)
+
+  if (loading) return <Splash />
+
+  return (
+    <SymProvider>
+      <SymAioHostContext value={symAioHost}>
+        <SymAioCtxContext.Provider
+          value={{
+            messages: cp!.full.messages,
+          }}
+        >
+          <SymMsgGroupContext value={symMsgGroupCtx}>
+            <SymMsgVirtualList />
+          </SymMsgGroupContext>
+        </SymAioCtxContext.Provider>
+      </SymAioHostContext>
+    </SymProvider>
+  )
+}
+
+const symAioHost: SymAioHost = {
+  frCanRemoveBubble: () => false,
+  frRenderers: {
+    ...frRenderers,
+  },
+}
+
+const symMsgGroupCtx: SymMsgGroupCtx = {
+  group: true,
+}
