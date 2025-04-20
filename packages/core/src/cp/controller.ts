30e8e792cec4ac7662efa0ead7dea8b4a185d4f5
Il Harper <hi@ilharper.com>
2025-04-20 19:47:48 +0800

    feat(core): update

--- packages/core/src/cp/controller.ts
+++ packages/core/src/cp/controller.ts
@@ -9,27 +9,31 @@ export const name = 'nekoil-cp-controller'
 export const inject = ['server', 'nekoilCp', 'nekoilUser']
 
 export const apply = (ctx: Context) => {
+  const l = ctx.logger('nekoilCpController')
+
   ctx.server.post('/nekoil/v0/cp/cp.get', async (c) => {
     // const user = await ctx.nekoilUser.getUser(platform, pid)
 
     const body = c.request.body as NekoilCpCpGetRequest
 
+    c.status = 200
     setHeader(c)
     c.flushHeaders()
 
-    const cfCountry = c.request.header['CF-IPCountry']
-    if (
-      !cfCountry ||
-      Array.isArray(cfCountry) ||
-      !cfCountry.length ||
-      cfCountry === 'CN'
-    ) {
-      c.body = {
-        code: 2002,
-        msg: 'EXXXXX FORBIDDEN',
-      }
-      return
-    }
+    l.info(c.request.header)
+    // const cfCountry = c.request.header['CF-IPCountry']
+    // if (
+    //   !cfCountry ||
+    //   Array.isArray(cfCountry) ||
+    //   !cfCountry.length ||
+    //   cfCountry === 'CN'
+    // ) {
+    //   c.body = {
+    //     code: 2002,
+    //     msg: 'EXXXXX FORBIDDEN',
+    //   }
+    //   return
+    // }
 
     c.body = await ctx.nekoilCp.cpGet(
       undefined as unknown as NekoilUser,
