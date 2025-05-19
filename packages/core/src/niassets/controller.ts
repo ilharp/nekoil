7d8f14c4968602a80df73abdd0ad8e46847de376
Il Harper <hi@ilharper.com>
2025-05-20 01:55:48 +0800

    feat(core): update controller

--- packages/core/src/niassets/controller.ts
+++ packages/core/src/niassets/controller.ts
@@ -21,6 +21,14 @@ export const apply = (ctx: Context, config: Config) => {
   ctx.server.all(
     '/nekoil/v0/proxy/internal\\:nekoil/2/:filename+',
     async (c, next) => {
+      if (c.method === 'OPTIONS') {
+        c.status = 200
+        setHeader(c)
+        c.set('Allow', 'GET,HEAD,OPTIONS')
+        c.flushHeaders()
+        return
+      }
+
       const nekoilProxyToken = c.request.header['nekoil-proxy-token']
       if (
         !nekoilProxyToken ||
@@ -28,10 +36,8 @@ export const apply = (ctx: Context, config: Config) => {
         !nekoilProxyToken.length ||
         nekoilProxyToken !== config.proxyToken
       ) {
-        c.body = {
-          code: 2003,
-          msg: 'EXXXXX FORBIDDEN',
-        }
+        c.status = 403
+        c.set('Cache-Control', 'no-store')
         return
       }
       const cfCountry = c.request.header['cf-ipcountry']
@@ -42,8 +48,11 @@ export const apply = (ctx: Context, config: Config) => {
         cfCountry === 'CN'
       ) {
         c.status = 403
+        c.set('Cache-Control', 'no-store')
         return
       }
+
+      setHeader(c, false)
 
       await next()
 
@@ -51,60 +60,51 @@ export const apply = (ctx: Context, config: Config) => {
         c.set('Cache-Control', 'public, max-age=60') // 1800
     },
     (c) => {
-      if (c.method === 'OPTIONS') {
-        c.status = 200
-        setHeader(c)
-        c.set('Allow', 'GET,HEAD,OPTIONS')
-        c.flushHeaders()
-        c.body = Buffer.allocUnsafe(0)
-        return
-      } else {
-        const filename = c.params['filename']!
+      const filename = c.params['filename']!
 
-        c.req.url = `/v1/${filename}`
-        ;(
-          c.req as unknown as {
-            body: unknown
-          }
-        ).body = c.request.body || null
+      c.req.url = `/v1/${filename}`
+      ;(
+        c.req as unknown as {
+          body: unknown
+        }
+      ).body = c.request.body || null
 
-        return new Promise<void>((resolve) => {
-          // https://github.com/nodejitsu/node-http-proxy/issues/951#issuecomment-179904134
-          c.res.on('close', () => {
-            // Sliently ignore close event.
-            resolve()
-          })
-
-          c.res.on('finish', () => {
-            resolve()
-          })
-
-          proxy.web(
-            c.req,
-            c.res,
-            {
-              target: config.assets.endpoint,
-              changeOrigin: true,
-            },
-            (e) => {
-              const status = (
-                {
-                  ECONNREFUSED: 503,
-                  ETIMEOUT: 504,
-                } as const
-              )[
-                (
-                  e as unknown as {
-                    code: number
-                  }
-                ).code
-              ]
-              c.status = status || 500
-              resolve()
-            },
-          )
+      return new Promise<void>((resolve) => {
+        // https://github.com/nodejitsu/node-http-proxy/issues/951#issuecomment-179904134
+        c.res.on('close', () => {
+          // Sliently ignore close event.
+          resolve()
         })
-      }
+
+        c.res.on('finish', () => {
+          resolve()
+        })
+
+        proxy.web(
+          c.req,
+          c.res,
+          {
+            target: config.assets.endpoint,
+            changeOrigin: true,
+          },
+          (e) => {
+            const status = (
+              {
+                ECONNREFUSED: 503,
+                ETIMEOUT: 504,
+              } as const
+            )[
+              (
+                e as unknown as {
+                  code: number
+                }
+              ).code
+            ]
+            c.status = status || 500
+            resolve()
+          },
+        )
+      })
     },
   )
 }
