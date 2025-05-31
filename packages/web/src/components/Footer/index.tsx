ce7c493d0abf9ca9861769404197eb236ffd2c0c
Il Harper <hi@ilharper.com>
2025-05-31 15:45:32 +0800

    feat(web): add Footer

---
+++ packages/web/src/components/Footer/index.tsx
@@ -0,0 +1,37 @@
+import type { ReactNode } from 'react'
+import styles from './index.module.scss'
+
+const handleOpenChannel = () => {
+  window.Telegram.WebApp.openTelegramLink('https://t.me/nekoil')
+}
+
+export const Footer = ({ children }: { children?: ReactNode }) => (
+  <div>
+    <div className={styles.container}>
+      {children}
+      {import.meta.env.MODE === 'development' && <p>开发模式</p>}
+      <p>Nekoil {__DEFINE_NEKOIL_VERSION_STRING__}</p>
+      <p>
+        在{' '}
+        <a
+          href="https://t.me/nekoil"
+          target="_blank"
+          rel="noreferrer noopener"
+          onClick={handleOpenChannel}
+        >
+          Nekoil
+        </a>{' '}
+        频道获得使用说明和更新通知
+      </p>
+      <p>
+        <a
+          href="https://github.com/ilharp/nekoil"
+          target="_blank"
+          rel="noreferrer noopener"
+        >
+          开放源代码
+        </a>
+      </p>
+    </div>
+  </div>
+)
