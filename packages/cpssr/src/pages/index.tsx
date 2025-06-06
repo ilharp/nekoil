feb13d889f0a92ddddbf96470af054dce61b969c
Il Harper <hi@ilharper.com>
2025-06-07 02:11:30 +0800

    feat(cpssr): add sym component

--- packages/cpssr/src/pages/index.tsx
+++ packages/cpssr/src/pages/index.tsx
@@ -1,3 +1,60 @@
-export default function Home() {
-  return <></>
+import h from '@satorijs/element'
+import type { SymAioHost, SymMsgGroupCtx } from '@sym-app/components'
+import {
+  frRenderers,
+  SymAioCtxContext,
+  SymAioHostContext,
+  SymMsgGroupContext,
+  SymMsgVirtualList,
+  SymProvider,
+} from '@sym-app/components'
+import type { ContentPackWithFull } from 'nekoil-typedef'
+import type { GetServerSideProps } from 'next'
+
+interface Props {
+  data: ContentPackWithFull
+}
+
+export default function Page({ data }: Props) {
+  return (
+    <SymProvider>
+      <SymAioHostContext value={symAioHost}>
+        <SymAioCtxContext.Provider
+          value={{
+            messages: data.full.messages.map((x) => ({
+              ...x,
+              symHeader: x.user?.name,
+              elements: h.parse(x.content!),
+            })),
+          }}
+        >
+          <SymMsgGroupContext value={symMsgGroupCtx}>
+            <SymMsgVirtualList />
+          </SymMsgGroupContext>
+        </SymAioCtxContext.Provider>
+      </SymAioHostContext>
+      <div />
+    </SymProvider>
+  )
+}
+
+export const getServerSideProps = (async (ctx) => {
+  return {
+    props: {
+      data: JSON.parse(
+        ctx.req.headers['nekoil-cpssr-data'] as string,
+      ) as ContentPackWithFull,
+    },
+  }
+}) satisfies GetServerSideProps<Props>
+
+const symAioHost: SymAioHost = {
+  frCanRemoveBubble: () => false,
+  frRenderers: {
+    ...frRenderers,
+  },
+}
+
+const symMsgGroupCtx: SymMsgGroupCtx = {
+  group: true,
 }
