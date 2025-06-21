4523b5d92fd101ee527534d99fad904f63232000
Il Harper <hi@ilharper.com>
2025-06-21 23:05:32 +0800

    feat(core): disable `nekoilimgrtest` command

--- packages/core/src/cp/imgr/index.ts
+++ packages/core/src/cp/imgr/index.ts
@@ -1,11 +1,11 @@
 import type { Context } from 'koishi'
 import type { Config } from '../../config'
-import * as commands from './commands'
+// import * as commands from './commands'
 import { NekoilCpImgrService } from './service'
 
 export const name = 'nekoil-cp-imgr'
 
 export const apply = (ctx: Context, config: Config) => {
   ctx.plugin(NekoilCpImgrService, config)
-  ctx.plugin(commands, config)
+  // ctx.plugin(commands, config)
 }
