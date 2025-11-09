723ad210de85b86cbabc0f026173fc0f720d0637
Il Harper <hi@ilharper.com>
2025-11-10 02:58:09 +0800

    feat(web): implement indexeddb persister

--- packages/web/src/App.tsx
+++ packages/web/src/App.tsx
@@ -1,5 +1,13 @@
 import { SymProvider } from '@sym-app/components'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import type { OmitKeyof } from '@tanstack/react-query'
+import { QueryClient } from '@tanstack/react-query'
+import type {
+  PersistedClient,
+  Persister,
+  PersistQueryClientOptions,
+} from '@tanstack/react-query-persist-client'
+import { PersistQueryClientProvider } from '@tanstack/react-query-persist-client'
+import { createStore, del, get, set } from 'idb-keyval'
 import { createBrowserRouter, RouterProvider } from 'react-router'
 import { Banned } from './pages/Banned'
 import { ContentPack } from './pages/ContentPack'
@@ -28,7 +36,8 @@ const router = createBrowserRouter([
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
-      staleTime: Infinity,
+      staleTime: 1000 * 60 * 60 * 24 * 7, // 7 days
+      gcTime: 1000 * 60 * 60 * 24 * 7, // 7 days
       retry: 3,
       refetchOnMount: false,
       refetchOnReconnect: false,
@@ -37,10 +46,37 @@ const queryClient = new QueryClient({
   },
 })
 
+export function createIDBPersister() {
+  const store = createStore('nekoil_v1', 'rq_v1')
+
+  return {
+    persistClient: async (client: PersistedClient) => {
+      await set('rq', client, store)
+    },
+    restoreClient: async () => {
+      return await get<PersistedClient>('rq', store)
+    },
+    removeClient: async () => {
+      await del('rq', store)
+    },
+  } satisfies Persister
+}
+
+const persister = createIDBPersister()
+
+const persistOptions: OmitKeyof<PersistQueryClientOptions, 'queryClient'> = {
+  persister,
+  maxAge: 1000 * 60 * 60 * 24 * 7, // 7 days
+  buster: 'rq_v1',
+}
+
 export const App = () => (
-  <QueryClientProvider client={queryClient}>
+  <PersistQueryClientProvider
+    client={queryClient}
+    persistOptions={persistOptions}
+  >
     <SymProvider className="sym-aio-msg-solidheader">
       <RouterProvider router={router} />
     </SymProvider>
-  </QueryClientProvider>
+  </PersistQueryClientProvider>
 )
