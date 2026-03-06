4b94799d0ad97617c42287dec8db3767772a3ca3
Il Harper <hi@ilharper.com>
2026-03-07 02:33:55 +0800

    feat(data): use `tracing`

--- packages/data-service/src/main.rs
+++ packages/data-service/src/main.rs
@@ -1,2 +1,28 @@
-pub fn main() {
+use std::process::ExitCode;
+
+use anyhow::Result;
+use tracing::error;
+
+fn main() -> ExitCode {
+    tracing_subscriber::fmt::init();
+
+    tokio::runtime::Builder::new_multi_thread()
+        .enable_all()
+        .build()
+        .unwrap()
+        .block_on(main_async())
+}
+
+async fn main_async() -> ExitCode {
+    match main_async_intl().await {
+        Ok(_) => ExitCode::SUCCESS,
+        Err(err) => {
+            error!("{err:?}");
+            ExitCode::FAILURE
+        }
+    }
+}
+
+async fn main_async_intl() -> Result<()> {
+    anyhow::Ok(())
 }
