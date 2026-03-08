11e280b4b7ad04a860128c781f5a79ed96ced851
Il Harper <hi@ilharper.com>
2026-03-09 01:14:58 +0800

    fix(data): fix dotenv path

--- packages/data-service/src/config.rs
+++ packages/data-service/src/config.rs
@@ -8,15 +8,11 @@ pub struct Config {
 }
 
 pub fn load_config(exe_dir: PathBuf) -> Result<Config> {
-    let env_map = EnvLoader::with_path(
-        exe_dir
-            .parent()
-            .unwrap()
-            .parent()
-            .unwrap()
-            .join(".env.local"),
-    )
-    .load()?;
+    let env_path = match exe_dir.parent().and_then(|p| p.parent()) {
+        Some(root) => root.join(".env.local"),
+        None => exe_dir.join(".env.local"),
+    };
+    let env_map = EnvLoader::with_path(env_path).load()?;
 
     Ok(Config {
         db_url: env_map.var("POSTGRES_URL")?,
